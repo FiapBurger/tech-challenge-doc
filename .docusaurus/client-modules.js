d48675c19477bff5aa27b06105099329bb8e688f
Fiapburger01 <fiapburger.tech@gmail.com>
2024-08-07 00:04:10 -0300

    teste 1

--- .docusaurus/client-modules.js
+++
@@ -1,6 +0,0 @@
-export default [
-  require('C:\\Users\\alexc\\OneDrive\\Área de Trabalho\\teste\\tech-challenge-doc\\node_modules\\infima\\dist\\css\\default\\default.css'),
-  require('C:\\Users\\alexc\\OneDrive\\Área de Trabalho\\teste\\tech-challenge-doc\\node_modules\\@docusaurus\\theme-classic\\lib\\prism-include-languages'),
-  require('C:\\Users\\alexc\\OneDrive\\Área de Trabalho\\teste\\tech-challenge-doc\\node_modules\\@docusaurus\\theme-classic\\lib\\nprogress'),
-  require('C:\\Users\\alexc\\OneDrive\\Área de Trabalho\\teste\\tech-challenge-doc\\src\\css\\custom.css'),
-];
