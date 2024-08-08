b7af39a7d8231606aeb54559f0e444066048d3be
FiapBurger <FiapBurger@users.noreply.github.com>
2024-08-08 01:37:42 +0000

    deploy: 0dfd65717f2e7b0fbb46766db3ca69b10747414b

--- assets/js/7d25e029.75f9a4ca.js
+++
@@ -1,1 +0,0 @@
-"use strict";(self.webpackChunktech_challenge_doc=self.webpackChunktech_challenge_doc||[]).push([[278],{7341:e=>{e.exports=JSON.parse('{"title":"Saga","description":"Padr\xe3o Utilizado","slug":"/category/saga","permalink":"/tech-challenge-doc/docs/category/saga","navigation":{"next":{"title":"Padr\xe3o SAGA - Coreografado","permalink":"/tech-challenge-doc/docs/SAGA/"}}}')}}]);
